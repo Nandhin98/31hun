e16ab0b52c1440d5c4e755bc64ee007332147cf5
Nandhin98 <47237438+Nandhin98@users.noreply.github.com>
2019-07-09 15:37:56 +0530

    Create 31.py

---
+++ 31.py
@@ -0,0 +1,20 @@
+ s = input()
+ L = list(permutations(s))
+ L2 = [ ''.join(x) for x in L]
+ L3 = list(set(L2))
+ #print(L3)
+ L4 = [ int(x) for x in L3]
+ if len(L4) == 1 :
+     print('impossible')
+     sys.exit()
+ n = int(s)
+ #print(L4)
+ max1 = max(L4)
+ if max1 <= n :
+     print('impossible')
+     sys.exit()
+ for i in range(0,len(L4)) :
+     if L4[i] > n :
+         if L4[i] < max1 :
+             max1 = L4[i]
+ print(max1)
